780f5383afab0645539a9b9b63b847134b26e970
Kaider <kronoz@yandex.ru>
2024-07-07 00:22:39 +0300

    Сделал пару фиктивных изменений для корректого залива на GitHub  и создания Pull Request

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,8 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+     static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+}
