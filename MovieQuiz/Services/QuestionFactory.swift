58f34c5926375124adf83e6332aa1ce0836bc7a0
Kaider <kronoz@yandex.ru>
2024-08-04 22:10:45 +0300

    Обновил метод requestNextQuestion

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -25,13 +25,41 @@ class QuestionFactory: QuestionFactoryProtocol {
                 self.movies = mostPopularMovies.items
                 self.delegate?.didLoadDataFromServer()
             case .failure(let error):
-                self.delegate?.didFailToLoadDataFromServer(with: error)
+                self.delegate?.didFailToLoadData(with: error)
+            }
+        }
+    }
+    func setup(delegate: QuestionFactoryDelegate) {
+        self.delegate = delegate
+    }
+    
+    func requestNextQuestion() {
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            
+            let index = (0..<self.movies.count).randomElement() ?? 0
+            guard let movie = self.movies[safe: index] else { return }
+            var imageData = Data()
+            do {
+                imageData = try Data(contentsOf: movie.imageURL)
+            } catch {
+                print("Failed to load image")
+            }
+            let rating = Float(movie.rating) ?? 0
+            let text = "Рейтинг этого фильма больше чем 7?"
+            let correctAnswer = rating > 7
+            
+            let question = QuizQuestion(image: imageData, text: text, correctAnswer: correctAnswer)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.delegate?.didReceiveNextQuestion(question: question)
             }
         }
     }
 }
 //    weak var delegate: QuestionFactoryDelegate?
-//    
+//
 //    private let questions: [QuizQuestion] = [
 //        QuizQuestion(image: "TheGodfather", text: "Рейтинг этого фильма больше чем 4?", correctAnswer: true),
 //        QuizQuestion(image: "TheDarkKnight", text: "Рейтинг этого фильма меньше чем 6?", correctAnswer: true),
@@ -44,11 +72,11 @@ class QuestionFactory: QuestionFactoryProtocol {
 //        QuizQuestion(image: "Tesla",text: "Рейтинг этого фильма больше чем 9?",correctAnswer: true),
 //        QuizQuestion(image: "Vivarium",text: "Рейтинг этого фильма меньше чем 3?",correctAnswer: false)
 //    ]
-//    
+//
 //    func setup(delegate: QuestionFactoryDelegate) {
 //        self.delegate = delegate
 //    }
-//    
+//
 //    func requestNextQuestion() -> QuizQuestion? {
 //        guard let index = (0..<questions.count).randomElement() else {
 //            delegate?.didReceiveNextQuestion(question: nil)
